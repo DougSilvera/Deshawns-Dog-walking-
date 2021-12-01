d5c5aacb29ee6578cd385c0cb29451e1680cec35
DougSilvera <shortxjdoug@gmail.com>
2021-12-01 11:54:52 -0500

    final click window added

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,14 @@
 import { getPets } from "./database.js"
+import { getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id = pets--${pet.id}>${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +16,27 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("pets")) {
+            const [,petId] = itemClicked.id.split("--")
+
+            for (const pet of pets) {
+                if (pet.id === parseInt(petId)) {
+
+                    for (const walker of walkers) {
+                        if (walker.id === pet.walkerId) {
+                            
+            
+                            
+                            window.alert(`${pet.name} is being walked by ${walker.name}`)
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+)
